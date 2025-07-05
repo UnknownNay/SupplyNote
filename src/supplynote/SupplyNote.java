fa34ab8a9e89c886b9b96b2c18f8161f53783bd9
Unknwonnay <aidonayaka4@gmail.com>
2025-07-05 20:39:26 +0700

    first commit

---
+++ src/supplynote/SupplyNote.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package supplynote;
+
+/**
+ *
+ * @author aido
+ */
+public class SupplyNote {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_utama utama = new Frame_utama();
+        utama.setVisible(true);
+    }
+}
